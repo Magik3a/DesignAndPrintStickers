42d0d43fabd04fed6bf70be7fd67d7f3d2133366
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-08-20 18:18:14 +0300

    Added on click to get modal with currently chosen template and paper size

--- DesignAndPrintStickers/Scripts/ClickEvents.js
+++ DesignAndPrintStickers/Scripts/ClickEvents.js
@@ -13,3 +13,14 @@ $(".template").click(function () {
 
     console.log("template click");
 });
+
+$("#btnAddImage").click(function () {
+    var templateName = $(".active-tempalte").attr("data-templatename");
+    var paperSizeName = $(".active-paper").attr("data-papersizename");
+    $.get('Home/GetAddImagesModal', { TemplateName: templateName,  PaperSize: paperSizeName}, function (data) {
+
+
+       });;
+
+   console.log("template click");
+});
