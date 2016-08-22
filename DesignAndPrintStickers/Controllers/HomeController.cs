1f44ba41b5e81279721284c90bb32b33f4f95851
Svetlin Krastanov <s.krastanov@televic.com>
2016-08-22 10:58:15 +0300

    .

--- DesignAndPrintStickers/Controllers/HomeController.cs
+++ DesignAndPrintStickers/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper.QueryableExtensions;
 using Data;
 using DataServices;
+using DesignAndPrintStickers.Helpers;
 using DesignAndPrintStickers.Models;
 using Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Web;
@@ -51,6 +54,42 @@ namespace DesignAndPrintStickers.Controllers
             return PartialView("ModalTemplates/AddImagesModal", model);
         }
 
+
+        [HttpPost]
+        public virtual ActionResult CropImage(
+     string imagePath,
+     int? cropPointX,
+     int? cropPointY,
+     int? imageCropWidth,
+     int? imageCropHeight)
+        {
+            if (string.IsNullOrEmpty(imagePath)
+                || !cropPointX.HasValue
+                || !cropPointY.HasValue
+                || !imageCropWidth.HasValue
+                || !imageCropHeight.HasValue)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+            string fileName = Path.GetFileName(imagePath);
+
+            try
+            {
+                FileHelper.SaveFile(croppedImage, Server.MapPath(imagePath));
+            }
+            catch (Exception ex)
+            {
+                //Log an error     
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+
+            string photoPath = string.Concat("/", fileName);
+            return Json(new { photoPath = imagePath }, JsonRequestBehavior.AllowGet);
+        }
         #region Mailing functions
 
         public ActionResult SentEmailAjax(string Email, string NameUser, string Subject, string Body)
---
+++ DesignAndPrintStickers/Helpers/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DesignAndPrintStickers.Helpers
+{
+
+    using System.Web.Hosting;
+    using System.IO;
+
+    public class FileHelper
+    {
+        public static void SaveFile(byte[] content, string path)
+        {
+            string filePath = GetFileFullPath(path);
+            if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
+
+            //Save file
+            using (FileStream str = System.IO.File.Create(filePath))
+            {
+                str.Write(content, 0, content.Length);
+            }
+        }
+
+        public static string GetFileFullPath(string path)
+        {
+            string relName = path.StartsWith("~") ? path : path.StartsWith("/") ? string.Concat("~", path) : path;
+
+            string filePath = relName.StartsWith("~") ? HostingEnvironment.MapPath(relName) : relName;
+
+            return filePath;
+        }
+    }
+}
